772db86926ff13f707d7f897a7590621ea09879a
unknown <clovissantannaa@gmail.com>
2024-12-17 17:06:36 -0300

    Fix .env

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,16 +1,16 @@
 
 
 import { getAnalytics } from "firebase/analytics";
-
+import Constants from 'expo-constants';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCVdB-CJs3HWHDSspVncKm-dGvBcE-ZV5I",
-    authDomain: "devtask-manager.firebaseapp.com",
-    projectId: "devtask-manager",
-    storageBucket: "devtask-manager.firebasestorage.app",
-    messagingSenderId: "808584292357",
-    appId: "1:808584292357:web:8dad739845429bf4c9d46c",
-    measurementId: "G-ZD4ZPGZDBZ"
+  apiKey: Constants.expoConfig?.extra?.firebaseApiKey,
+  authDomain: Constants.expoConfig?.extra?.firebaseAuthDomain,
+  projectId: Constants.expoConfig?.extra?.firebaseProjectId,
+  storageBucket: Constants.expoConfig?.extra?.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig?.extra?.firebaseMessagingSenderId,
+  appId: Constants.expoConfig?.extra?.firebaseAppId,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   };
 
 export {firebaseConfig};
